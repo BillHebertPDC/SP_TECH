d482e8ca6b48e2d15c15cda83c49d4f30c71889f
BillHebertPDC <bill.choi@sptech.school.com>
2025-04-06 21:51:49 -0300

    06-04

--- Faculdade/PI/Umido/server/public/s.js
+++ Faculdade/PI/Umido/server/public/s.js
@@ -98,3 +98,152 @@ window.onload = () => {
 
 
 //for(;true;){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let dados = {
+    montar_grafico: (tipo, labels, dados, opcoes) => {
+        return {
+            type: tipo,
+            data: {
+                labels: labels,
+                datasets: dados
+            },
+            options: opcoes
+        }
+    },
+    opcoes_padrão: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    },
+    grupos_sensores: {
+        temperatura: {
+            label: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+            dados: [{
+                label: 'Umidade',
+                data: [35, 33, 30, 35, 40],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                fill: true
+            },
+            {
+                label: 'Temperatura',
+                data: [25, 26, 30, 28, 20],
+                borderColor: 'rgba(255, 192, 192, 1)',
+                backgroundColor: 'rgba(255, 192, 192, 0.2)',
+                fill: true
+            }]
+        },
+        tempo_real: {
+            label: ['40s', '35s', '30s', '25s', '20s', '15s', '10s', '5s'],
+            dados: [{
+                label: 'Temperatura',
+                data: [31, 31, 31, 31, 30.5, 30, 30, 30],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                fill: true
+            }, {
+                label: 'umidade',
+                data: [28, 28, 28, 29, 30, 31, 31, 30.5],
+                borderColor: 'rgba(255, 192, 192, 1)',
+                backgroundColor: 'rgba(255, 192, 192, 0.2)',
+                fill: true
+            }]
+        },
+        analise: {
+            label: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            dados: [{
+                label: '2024',
+                data: ['120', '110', '100', '90', '80', '70', '60', '50', "40", "30", "20", "10"],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                fill: true
+            }]
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = () => {
+    carregarHTML('conteudo', 'layout/dashboard.html')
+    carregarHTML('nav', 'layout/nav.html');
+    carregarHTML('list_sensor', 'componentes/list_sensor.html');
+    carregarHTML('graph_sensor', 'componentes/graph_sensor.html', {
+        ["class-principal"]: "sensor-graph",
+        ["class-conteiner-grafico"]: "grafico-container",
+        ["class-grafico"]: "grafico-wrapper",
+        ["id-grafico"]: "grafico_grupo_sensor",
+        titulo: "Media Grupo Sensor (Mês)",
+        texto1: "temperatura",
+        texto2: "umidade",
+        texto3: "media",
+        ["dados_grafico"]: JSON.stringify(dados.montar_grafico("line", dados.grupos_sensores.temperatura.label, dados.grupos_sensores.temperatura.dados, dados.opcoes_padrão))
+    });
+    carregarHTML('list_alerta', 'componentes/alerts.html');
+    carregarHTML('graph_sensor_individual', 'componentes/graph_sensor.html', {
+        ["class-principal"]: "sensor-graph",
+        ["class-conteiner-grafico"]: "grafico-container",
+        ["class-grafico"]: "grafico-wrapper",
+        ["id-grafico"]: "grafico_grupo_sensor2",
+        titulo: "Sensores (Tempo Real)",
+        texto1: "temperatura",
+        texto2: "umidade",
+        texto3: "media",
+        ["dados_grafico"]: JSON.stringify(dados.montar_grafico("line", dados.grupos_sensores.tempo_real.label, dados.grupos_sensores.tempo_real.dados, dados.opcoes_padrão))
+    });
+    carregarHTML("graph_incidentes_mes", 'componentes/graph_sensor.html', {
+        ["class-principal"]: "sensor-graph",
+        ["class-conteiner-grafico"]: "grafico-container",
+        ["class-grafico"]: "grafico-wrapper",
+        ["id-grafico"]: "grafico_analise",
+        titulo: "Analise de eventos anual",
+        texto1: "Quantidade de Queimadas",
+        texto2: "",
+        texto3: "",
+        ["dados_grafico"]: JSON.stringify(dados.montar_grafico("line", dados.grupos_sensores.analise.label, dados.grupos_sensores.analise.dados, dados.opcoes_padrão))
+    })
+
+};
